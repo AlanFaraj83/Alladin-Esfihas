24ab5d2e8b76aa89b23ad16edf4130266d4603a0
AlanFaraj83 <alanfaraj@outlook.com>
2023-02-16 21:19:57 -0300

    Atualizando o projeto e o script.js

--- esfihas.js
+++ esfihas.js
@@ -1,10 +1,10 @@
 let esfihaJson = [
-    {id:1, name:'Carne', img:'images/esfiha carne.jpg', price:25.00, sizes:['100g', '530g', '860g'], description:'carne, cebola, tomate, orégano'},
-    {id:2, name:'Calabresa', img:'images/esfiha calabresa.jpg', price:26.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, calabresa fatiada, cebola e orégano'},
-    {id:3, name:'Quatro Queijos', img:'images/esfiha quatro queijos.jpg', price:28.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
-    {id:4, name:'Frango Catupiry', img:'images/esfiha frango catupiry.jpg', price:30.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, frango, catupiry, palmito e orégano'},
-    {id:5, name:'Mussarela', img:'images/esfiha mussarela.jpg', price:29.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela e orégano'},
-    {id:6, name:'Palmito', img:'images/esfiha palmito.jpeg', price:35.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, palmito, coentro, cebola e orégano'},
-    {id:7, name:'escarola', img:'images/esfiha escarola catupiry.jpeg', price:32.00, sizes:['320g', '530g', '860g'], description:'escarola, cebola, catupiry, molho de tomate'},
-    {id:8, name:'brigadeiro', img:'images/esfiha brigadeiro.jpeg', price:35.00, sizes:['320g', '530g', '860g'], description:'Creme de leite, chocolate, granulados de chocolate '}
+    {id:1, name:'Carne', img:'images/esfiha carne.jpg', price: 25.00, sizes:['100g', '530g', '860g'], description:' Molho de tomate, carne, cebola, tomate, orégano'},
+    {id:2, name:'Calabresa', img:'images/esfiha calabresa.PNG', price:26.00, sizes:['320g', '530g', '860g'], description:' Molho de tomate, calabresa fatiada, cebola e orégano'},
+    {id:3, name:'Quatro Queijos', img:'images/esfiha quatro queijos.PNG', price: 28.00, sizes:['320g', '530g', '860g'], description:' Molho de tomate, camadas de mussarela, provolone, parmessão, gorgonzola e orégano'},
+    {id:4, name:'Frango Catupiry', img:'images/esfiha frango catupiry.jpg', price: 30.00, sizes:['320g', '530g', '860g'], description:' Molho de tomate, frango, catupiry, palmito e orégano'},
+    {id:5, name:'Mussarela', img:'images/esfiha mussarela.jpg', price: 29.00, sizes:['320g', '530g', '860g'], description:' Molho de tomate, mussarela e orégano'},
+    {id:6, name:'Palmito', img:'images/esfiha palmito.PNG', price: 35.00, sizes:['320g', '530g', '860g'], description:' Molho de tomate, palmito, coentro, cebola e orégano'},
+    {id:7, name:'escarola', img:'images/esfiha escarola catupiry.PNG', price: 32.00, sizes:['320g', '530g', '860g'], description:' escarola, cebola, catupiry, molho de tomate'},
+    {id:8, name:'brigadeiro', img:'images/esfiha brigadeiro.jpeg', price: 35.00, sizes:['320g', '530g', '860g'], description:' Creme de leite, chocolate, granulados de chocolate '}
 ];
--- js/script.js
+++ js/script.js
@@ -1,33 +1,70 @@
-esfihaJson.map((item, index ) => {
-    //console.log(item)
-    let esfihaItem = document.querySelector('.models .esfiha-item').cloneNode(true)
-    //console.log(esfihaItem)
-    document.querySelector('.esfiha-area').append(esfihaItem) 
+// funcoes auxiliares ou uteis
+const seleciona = (elemento) => document.querySelector(elemento)
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento)
 
-    // preencher os dados de cada pizza
+const abrirModal = () => {
+    seleciona('.esfihaWindowArea').style.opacity = 0
+    seleciona('.esfihaWindowArea').style.display = 'flex'
+    setTimeout(() => {
+        seleciona('.esfihaWindowArea').style.opacity = 1
+    }, 150)
+}
+
+const fecharModal = () => {
+    seleciona('.esfihaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        seleciona('.esfihaWindowArea').style.display = 'none'
+    }, 500)
+}
+
+const botoesFechar = () => {
+    
+    
+    // BOTOES FECHAR MODAL
+    selecionaTodos('.esfihaInfo--cancelButton, .esfihaInfo--cancelMobileButton').forEach((item) => {
+        item.addEventListener('click', fecharModal)
+    })
+}
+
+const preencheDadosDasEsfihas = (esfihaItem, item) => {
     esfihaItem.querySelector('.esfiha-item--img img').src = item.img
     esfihaItem.querySelector('.esfiha-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     esfihaItem.querySelector('.esfiha-item--name').innerHTML = item.name
     esfihaItem.querySelector('.esfiha-item--desc').innerHTML = item.description
+}
 
-    // Quando a esfiha for clicada
+const preencheDadosModal = (item) => {
+    
+    seleciona('.esfihaBig img').src = item.img
+    seleciona('.esfihaInfo h1').innerHTML = item.name
+    seleciona('.esfihaInfo--desc').innerHTML = item.description
+    seleciona('.esfihaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
+}
+
+// MAPEAR esfihaJson para gerar lista de esfihas
+esfihaJson.map((item, index ) => {
+    //console.log(item)
+    let esfihaItem = document.querySelector('.models .esfiha-item').cloneNode(true)
+    
+    seleciona('.esfiha-area').append(esfihaItem)
+
+    // preencher os dados de cada esfiha
+    preencheDadosDasEsfihas(esfihaItem, item)
+    
+    // pizza clicada
     esfihaItem.querySelector('.esfiha-item a').addEventListener('click', (e) => {
         e.preventDefault()
-        console.log('clicou na esfiha')
+        console.log('Clicou na esfiha')
 
-        // Abrir janela modal
-        document.querySelector('.esfihaWindowArea').style.display = 'flex'
+        // abrir janela modal
+        
+        abrirModal()
+
+        // preenchimento dos dados
+        preencheDadosModal(item)
 
     })
 
-    document.querySelector('.esfihaInfo--cancelButton').addEventListener('click', () => {
-        document.querySelector('.esfihaWindowArea').style.display = 'none'
-    })
+    botoesFechar()
 
-
-    document.querySelector('.esfihaInfo--cancelMobileButton').addEventListener('click', () => {
-        document.querySelector('.esfihaWindowArea').style.display = 'none'
-    })
-
-
-})
+}) // fim do MAPEAR pizzaJson para gerar lista de esfihas
